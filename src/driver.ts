f7d1dfbd65ebd99479156879a957d0e996d96064
Adam Cowley <github@adamcowley.co.uk>
2021-02-16 10:09:37 +0000

    Expose driver config in createDriver function

--- src/driver.ts
+++ src/driver.ts
@@ -1,11 +1,11 @@
 import { Neo4jScheme } from "./neo4j-config.interface"
 
-import neo4j from 'neo4j-driver'
+import neo4j, { Config } from 'neo4j-driver'
 
-export const createDriver = (scheme: Neo4jScheme, host: string, port: string | number, username?: string, password?: string) => {
+export const createDriver = (scheme: Neo4jScheme, host: string, port: string | number, username?: string, password?: string, config?: Config) => {
     if ( !username || !password ) {
         return neo4j.driver(`${scheme}://${host}:${port}`)
     }
 
-    return neo4j.driver(`${scheme}://${host}:${port}`, neo4j.auth.basic(username, password))
+    return neo4j.driver(`${scheme}://${host}:${port}`, neo4j.auth.basic(username, password), config)
 }
